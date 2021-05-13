0aec009dc92f610758a2824b35439c3858a89a10
juyeongjung <juyeong.dev@gmail.com>
2021-05-13 18:52:25 +0900

    feat: init server project

---
+++ server/src/main/java/com/db/datastoreserver/DatastoreserverApplication.java
@@ -0,0 +1,13 @@
+package com.db.datastoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastoreserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatastoreserverApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/db/datastoreserver/DatastoreserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.datastoreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastoreserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
