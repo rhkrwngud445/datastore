71eb0eae5cf4adc5eb78a9563315b5cd37a5f5c3
rhkrwngud445 <rhkrwngud445@naver.com>
2021-05-24 15:37:58 +0900

    feat:홈activity recycler view 추가

--- app/src/main/java/com/db/datastore/HomeRvAdapter.kt
+++ app/src/main/java/com/db/datastore/HomeRvAdapter.kt
@@ -1,2 +1,63 @@
 package com.db.datastore
 
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+
+class HomeRvAdapter (private val items: ArrayList<recyclerList>) : RecyclerView.Adapter<HomeRvAdapter.ViewHolder>() {
+//
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+        private var view : View = v
+
+        val textview : TextView? = null
+
+        fun bind(listener:View.OnClickListener,item: recyclerList) {
+
+//            tv_name.text= item.name
+//            tv_minPrice.text= item.minPrice.toString()
+//            tv_deliveryTip.text= item.deliveryTip.toString()
+//            tv_deliveryTime.text= item.deliverTime.toString()
+            view.setOnClickListener(listener)
+            //view..setImageDrawable(item.image)
+            //tv_name.text = item.name.setOnClickListener(listener)
+        }
+
+        init
+        {
+//            layout = view.findViewById(R.id.rc_layout)
+//            tv_name = view.findViewById(R.id.rc_tv_name)
+//            tv_deliveryTime = view.findViewById(R.id.rc_tv_deliverytime)
+//            tv_deliveryTip = view.findViewById(R.id.rc_tv_deliveytip)
+//            tv_minPrice = view.findViewById(R.id.rc_tv_minprice)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val inflatedView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.home_recycler_item, parent, false)
+            return ViewHolder(inflatedView)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        // listener -> 클릭시 이벤트를 넣고 싶을 때
+
+        val listener = View.OnClickListener { it->
+
+        }
+        holder.apply {
+            bind(listener,item)
+            itemView.tag= item
+        }
+    }
+
+
+}
--- app/src/main/java/com/db/datastore/MainActivity.kt
+++ app/src/main/java/com/db/datastore/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
+
+data class recyclerList(val private : String)
