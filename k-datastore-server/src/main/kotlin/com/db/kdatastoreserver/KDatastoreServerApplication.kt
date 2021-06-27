dcb6652e9101cd6265090811656a3c5b5894fd34
juyeongjung <juyeong.dev@gmail.com>
2021-06-27 17:10:34 +0900

    feat: default 사용자값 추가

--- k-datastore-server/src/main/kotlin/com/db/kdatastoreserver/KDatastoreServerApplication.kt
+++ k-datastore-server/src/main/kotlin/com/db/kdatastoreserver/KDatastoreServerApplication.kt
@@ -1,10 +1,25 @@
 package com.db.kdatastoreserver
 
+import com.db.kdatastoreserver.domain.Member
+import com.db.kdatastoreserver.domain.repository.MemberRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.mongodb.core.MongoTemplate
+
+const val DEFAULT_IMAGE: String = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
 
 @SpringBootApplication
-class KDatastoreServerApplication
+class KDatastoreServerApplication {
+    @Bean
+    fun init(
+        repository: MemberRepository,
+        template: MongoTemplate
+    ) = CommandLineRunner {
+        repository.findByName("데데시") ?: repository.save(Member("데데시", DEFAULT_IMAGE))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KDatastoreServerApplication>(*args)
